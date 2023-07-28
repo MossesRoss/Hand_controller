82f24153671e1e20f3b75ead0b67123afd36d19e
Mosses Ross <mosasross@gmail.com>
2023-07-28 13:37:29 +0530

    Update volume_control.py
    Added flexibility, inverted the volume changing axis, green line indication, removed hand drawing and added comments

--- volume_control.py
+++ volume_control.py
@@ -7,7 +7,6 @@ import subprocess
 def change_volume():
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
-    mp_drawing = mp.solutions.drawing_utils
 
     cap = cv2.VideoCapture(0)
     screen_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -30,8 +29,6 @@ def change_volume():
 
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
                 index_finger_landmark = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
                 index_finger_x = int(index_finger_landmark.x * screen_width)
                 index_finger_y = int(index_finger_landmark.y * screen_height)
@@ -43,16 +40,23 @@ def change_volume():
                     return
 
                 # Calculate the new volume
-                new_volume = 1.0 - (index_finger_y / screen_height)
-                if new_volume < 0.0:
-                    new_volume = 0.0
-                elif new_volume > 1.0:
-                    new_volume = 1.0
+                top_margin = 10
+                bottom_margin = 10
+                max_volume_y = screen_height - top_margin
+                min_volume_y = bottom_margin
+                new_volume = (index_finger_y - min_volume_y) / (max_volume_y - min_volume_y)
+                new_volume = max(0.0, min(1.0, new_volume))
 
                 # Set the volume if it's changed
                 if new_volume != current_volume:
                     volume.SetMasterVolumeLevelScalar(new_volume, None)
                     current_volume = new_volume
+
+                # Square around the index finger
+                cv2.rectangle(image, (index_finger_x - 10, index_finger_y - 10), (index_finger_x + 10, index_finger_y + 10), (255, 255, 255), 2)
+
+                # Horizontal line where the index finger is
+                cv2.line(image, (0, index_finger_y), (screen_width, index_finger_y), (0, 255, 0), 3)
 
         cv2.imshow('User Interface', image)
 
@@ -63,6 +67,6 @@ def change_volume():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    print("Move your finger down to decrease the volume and do the opposite to increase the volume.")
-    print("To close this [press space bar] and go to the change_modes_template, move your index finger to the left.")
+    print("Move your finger down to increase the volume and do the opposite to decrease the volume.")
+    print("To close this and go to the change modes template, move your finger to the left.")
     change_volume()
